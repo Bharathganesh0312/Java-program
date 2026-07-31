9ceeab4c4cccdba689f4ede30aeda4310301fb49
Bharathganesh0312 <bharathganesh69@gmail.com>
2026-07-31 14:12:20 +0530

    Add files via upload

---
+++ Matrix.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Matrix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int rows = sc.nextInt();
+        System.out.print("Enter the number of column: ");
+        int cols = sc.nextInt();
+        int[][] matrix = new int[rows][cols];
+
+        System.out.println("Enter the matrix elements:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("The matrix is:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
